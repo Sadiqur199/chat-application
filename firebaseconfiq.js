006852d6da6b1557822b2a79aa84eb6b39b131e8
sadiq <sadiqurrhaman199@gmail.com>
2024-11-01 00:51:23 +0600

    hide firebase api key

--- firebaseconfiq.js
+++ firebaseconfiq.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwC6QpjlEAt6YiJcYQnjo5EJcHpNQBOn4",
-  authDomain: "chat-application-21cb3.firebaseapp.com",
-  projectId: "chat-application-21cb3",
-  storageBucket: "chat-application-21cb3.firebasestorage.app",
-  messagingSenderId: "612462323469",
-  appId: "1:612462323469:web:7d1fb0013754cf4f36b244"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket:  import.meta.env.VITE_storageBucke,
+  messagingSenderId:  import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_apiKey,
 };
 
 const app = initializeApp(firebaseConfig);
